0b6c89e0474fb88f1fad85c6c8654b1b64e6f6ad
Oscar-ndegwa <ndegwaoscar9@gmail.com>
2026-05-25 23:53:20 +0300

    Set base path for Vite configuration
    Added base path configuration for repository name.

--- agape-hybrid/vite.config.ts
+++ agape-hybrid/vite.config.ts
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // Add your repository name wrapped in slashes here:
+  base: "/your-repository-name/", 
   build: { outDir: "dist" },
 });
